86081e593d972473a4aaf0f54598c29851b2a597
Jason Leach <jlspursfan@gmail.com>
2019-08-23 14:38:35 -0700

    added Post model w/ validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_23_194723) do
+ActiveRecord::Schema.define(version: 2019_08_23_212622) do
+
+  create_table "posts", force: :cascade do |t|
+    t.date "date"
+    t.text "rationale"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
---
+++ spec/models/post_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  describe "Creation" do
+    before do
+      @post = Post.create(date: Date.today, rationale: "Whatevs")
+    end
+
+    it "can be created" do
+      expect(@post).to be_valid
+    end
+
+    it "cannot be created w/out date & rationale" do
+      @post.date = nil
+      @post.rationale = nil
+      expect(@post).to_not be_valid
+    end
+  end
+end
