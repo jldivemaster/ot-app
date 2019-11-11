b49b46a5c51438563cb23afe4fcd12658e637ddf
Jason Leach <jlspursfan@gmail.com>
2019-11-11 10:59:39 -0800

    paginated uadit logs & posts indexes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,5 +3,24 @@ module ApplicationHelper
   def active?(path)
     "active" if current_page?(path)
   end
-  
+
+  def status_label(status)
+    status_code_generator status
+  end
+
+  def status_code_generator status
+    case status
+    when 'submitted'
+      content_tag(:span, status.titleize, class: 'label label-primary')
+    when 'approved'
+      content_tag(:span, status.titleize, class: 'label label-success')
+    when 'rejected'
+      content_tag(:span, status.titleize, class: 'label label-danger')
+    when 'pending'
+      content_tag(:span, status.titleize, class: 'label label-primary')
+    when 'confirmed'
+      content_tag(:span, status.titleize, class: 'label label-success')
+    end
+  end
+
 end
